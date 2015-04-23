58c9e33926e37cf27ed5a7a4d0c7f2a95f4133e3
Donovan Tengblad <purplefish32@gmail.com>
2015-04-23 15:26:15 +0200

    Cleanup

--- index.js
+++ index.js
@@ -28,69 +28,19 @@ app.post('/apps/:app_id/events', function(req, res) {
   }
 
   //TODO better response
-  res.send('OK');
+  res.send('{}');
 });
 
 io.on('connection', function (socket) {
   console.log('Client connected');
 
-/*
-    emitServerData({
-      serverUID:    event.data.serverId,
-      diskTotal:    details.disk.total,
-      diskUsed:     details.disk.used,
-      diskFree:     details.disk.free,
-      cpuCount:     details.cpu.count,
-      cpuLoadMin1:  details.cpu.load.min1,
-      cpuLoadMin5:  details.cpu.load.min5,
-      cpuLoadMin15: details.cpu.load.min15,
-      memoryTotal:  details.memory.total,
-      memoryUsed:   details.memory.used,
-      memoryFree:   details.memory.free,
-      swapTotal:    details.memory.swap.total,
-      swapUsed:     details.memory.swap.used,
-      swapFree:     details.memory.swap.free
-    });*/
+  var subscriptions = socket.handshake.query.channels.split(',');
 
-  // when the client emits 'notification', this listens and executes
-  socket.on('notification', function () {
-    console.log('On notification');
-    emitNotification({
-      type: 'info',
-      title: 'hi',
-      message: 'hi'
-    });
+  // Join rooms
+  subscriptions.forEach(function(room){
+    socket.join(room);
+    console.log("Joined : " + room);
   });
-
-  function emitNotification(data) { 
-    console.log('Emit notification');
-    socket.emit('notification', {
-      type: data.type || 'info',
-      title: data.title,
-      message: data.message
-    });
-  }
-
-  function emitServerData(data) { 
-    console.log('emitting server data');
-    console.log(data);
-    socket.emit('serverData', {
-      serverUID:    data.serverUID    || 'Unknown',
-      diskTotal:    data.diskTotal    || 'Unknown',
-      diskUsed:     data.diskUsed     || 'Unknown',
-      diskFree:     data.diskFree     || 'Unknown',
-      cpuCount:     data.cpuCount     || 'Unknown',
-      cpuLoadMin1:  data.cpuLoadMin1  || 'Unknown',
-      cpuLoadMin5:  data.cpuLoadMin5  || 'Unknown',
-      cpuLoadMin15: data.cpuLoadMin15 || 'Unknown',
-      memoryTotal:  data.memoryTotal  || 'Unknown',
-      memoryUsed:   data.memoryUsed   || 'Unknown',
-      memoryFree:   data.memoryFree   || 'Unknown',
-      swapTotal:    data.swapTotal    || 'Unknown',
-      swapUsed:     data.swapUsed     || 'Unknown',
-      swapFree:     data.swapFree     || 'Unknown'
-    });
-  }
 
   // when the user disconnects.. perform this
   socket.on('disconnect', function () {
