47fd51d1f2c76c99abe1276dd14525a033497f93
Donovan Tengblad <purplefish32@gmail.com>
2015-04-28 13:43:38 +0200

    Added debug messages

--- index.js
+++ index.js
@@ -11,19 +11,22 @@ app.use(bodyParser.json());
 server.listen(port);
 
 io.on('connection', function (socket) {
+  console.log('Connection');
   var channels = socket.handshake.query.channels.split(',');
 
   channels.forEach(function(channel){
+    console.log('Join chanel : ' + channel);
     socket.join(channel);
   });
 });
 
 app.post('/apps/:app_id/events', function(req, res) {
-
+  console.log('Recieve post request');
   var channels = req.body.channels;
   var name     = req.body.name;
   var data     = req.body.data;
 
+  console.log('Broadcast');
   if (channels) {  
     channels.forEach(function(channel){
       io.sockets.in(channel).emit(name, data);
